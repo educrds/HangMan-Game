d70d7c46972ba1d1d4750ae406994295299b76da
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-10-06 12:38:16 -0300

    fixed hangman animation

--- public/js/script.js
+++ public/js/script.js
@@ -1,115 +1,18 @@
-const canvas = document.querySelector("#canvas");
+let tries;
+let wordDisplay;
+let winCheck;
+// -------------------  CANVAS SETUP ---------------------------------------------
+const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-function canva() {
-  ctx.fillStyle = "#010b2f";
-  ctx.strokeStyle = "#010b2f";
-  ctx.lineWidth = 10;
-  ctx.lineCap = "round";
-}
-// if (!canvas.getContext) {
-//   return;
-// }
-function head() {
-  ctx.beginPath();
-  ctx.arc(200, 80, 30, 0, Math.PI * 2, true);
-  ctx.fill();
-}
-
-function draw(fromX, fromY, toX, toY) {
-  ctx.moveTo(fromX, fromY);
-  ctx.lineTo(toX, toY);
-  ctx.stroke();
-}
-function verticalTrunk() {
-  draw(30, 10, 30, 300);
-}
-function horizontalTrunk() {
-  draw(10, 10, 220, 10);
-}
-function diagonalTrunk() {
-  draw(30, 60, 80, 10);
-}
-function rope() {
-  draw(200, 10, 200, 50);
-}
-function body() {
-  draw(200, 100, 200, 200);
-}
-function armRight() {
-  draw(200, 120, 250, 150);
-}
-function armLeft() {
-  draw(200, 120, 150, 150);
-}
-function legRight() {
-  draw(200, 200, 170, 260);
-}
-function legLeft() {
-  draw(200, 200, 230, 260);
-}
-
-// // tronco vertical
-// ctx.beginPath();
-// ctx.moveTo(30, 10);
-// ctx.lineTo(30, 300);
-// ctx.stroke();
-// // tronco horizontal
-// ctx.beginPath();
-// ctx.moveTo(10, 10);
-// ctx.lineTo(220, 10);
-// ctx.stroke();
-// // tronco angular
-// ctx.beginPath();
-// ctx.moveTo(30, 60);
-// ctx.lineTo(80, 10);
-// ctx.stroke();
-// //  corda
-// ctx.beginPath();
-// ctx.moveTo(200, 10);
-// ctx.lineTo(200, 50);
-// ctx.stroke();
-// Body
-// ctx.beginPath();
-// ctx.moveTo(200, 100);
-// ctx.lineTo(200, 200);
-// ctx.stroke();
-
-// Arm Right
-// ctx.beginPath();
-// ctx.moveTo(200, 120);
-// ctx.lineTo(250, 150);
-// ctx.stroke();
-
-// // Arm Left
-// ctx.beginPath();
-// ctx.moveTo(200, 120);
-// ctx.lineTo(150, 150);
-// ctx.stroke();
-// Leg Right
-// ctx.beginPath();
-// ctx.moveTo(200, 200);
-// ctx.lineTo(170, 260);
-// ctx.stroke();
-
-// // Leg Left
-// ctx.beginPath();
-// ctx.moveTo(200, 200);
-// ctx.lineTo(230, 260);
-// ctx.stroke();
-
-var drawArray = [
-  legRight,
-  legLeft,
-  body,
-  armLeft,
-  armRight,
-  head,
-  rope,
-  horizontalTrunk,
-  diagonalTrunk,
-  verticalTrunk,
-];
+// ------------------- ID`s ------------------------------------------------------
+const categoryText = document.getElementById("categoryLabel");
+const wordText = document.getElementById("word");
+const buttonVerify = document.getElementById("button-addon2");
+const inputValue = document.getElementById("inputLetter");
+const triesNumber = document.getElementById("triesNumber");
+const triesLetters = document.getElementById("tries");
+const restartBtn = document.getElementById("restart");
+const categoryChoice = localStorage.getItem("id");
 
 const Words = [
   {
@@ -217,99 +120,150 @@ if (document.body.id === "start") {
   document.querySelector("#Esportes").onclick = () => {
     localStorage.setItem("id", "Esportes");
   };
-  document.querySelector("#Aleatorio").onclick = () => {
-    localStorage.setItem("id", "Aleatorio");
-  };
 }
-// ------------------- GAME PAGE ---------------------------------------------
+
+
+
 init();
 
 function init() {
-  if (document.body.id === "game") {
-    const categoryText = document.getElementById("categoryLabel");
-    const wordText = document.getElementById("word");
-    const buttonVerify = document.getElementById("button-addon2");
-    const inputValue = document.getElementById("inputLetter");
-    const triesNumber = document.getElementById("triesNumber");
-    const triesLetters = document.getElementById("tries");
-    const restartBtn = document.getElementById("restart");
-    const categoryChoice = localStorage.getItem("id");
+  ctx.clearRect(0, 0, 300, 300);
+  canva();
+  tries = 10;
+  winCheck = "";
+  triesLetters.innerHTML = "";
+  triesNumber.innerHTML = `Restam ${tries} tentativas`;
+  categoryText.innerHTML = categoryChoice;
+  let status = false;
 
-    categoryText.innerHTML = categoryChoice;
 
-    canva();
+// ------------------- GAME PAGE ---------------------------------------------
+if (document.body.id === "game") {
 
-    triesLetters.innerHTML = "";
+  let categoryFiltered = Words.filter(
+    (word) => word.category === categoryChoice
+  );
+  let randomWord =
+    categoryFiltered[Math.floor(Math.random() * categoryFiltered.length)].word;
+  // console.log(randomWord);
 
-    let categoryFiltered = Words.filter(
-      (word) => word.category === categoryChoice
-    );
-    let randomWord =
-      categoryFiltered[Math.floor(Math.random() * categoryFiltered.length)]
-        .word;
-    // console.log(randomWord);
+  let wordDisplay = [];
 
-    let wordDisplay = [];
-
-    function generateAnswerDisplay(word) {
-      let wordArray = word.split("");
-      for (let i = 0; i < randomWord.length; i++) {
-        if (wordArray[i] !== "-") {
-          wordDisplay.push("_");
-        } 
+  function generateAnswerDisplay(word) {
+    let wordArray = word.split("");
+    for (let i = 0; i < randomWord.length; i++) {
+      if (wordArray[i] !== "-") {
+        wordDisplay.push("_");
       }
-      return wordDisplay.join(" ");
+    }
+    return wordDisplay.join(" ");
+  }
+
+  wordText.innerHTML = generateAnswerDisplay(randomWord);
+  triesLetters, (innerHTML = "");
+
+  buttonVerify.onclick = () => {
+    const answerArray = randomWord.split("");
+    status = false
+    for (let j = 0; j < randomWord.length; j++) {
+      if (inputValue.value.toLowerCase() === answerArray[j]) {
+        wordDisplay[j] = inputValue.value.toLowerCase();
+        wordText.innerHTML = wordDisplay.join(" ");
+        winCheck = wordDisplay.join("");
+        console.log(winCheck);
+        status = true;
+      }
+    }
+    if (!status) {
+      triesLetters.innerHTML += inputValue.value.toLowerCase();
+      tries--;
+      drawArray[tries]();
+      // animateMan();
+
+      triesNumber.innerHTML = `Restam ${tries} tentativas`;
+    }
+    if (tries == 0) {
+      triesNumber.innerHTML = "";
+      wordText.innerHTML = "Você perdeu :(";
+      triesNumber.innerHTML = `A palavra era ${randomWord}`;
+    }
+    if (winCheck === randomWord) {
+      wordText.innerHTML = "Ihuul!! Você ganhou :D";
+      triesNumber.innerHTML = "";
     }
 
-    wordText.innerHTML = generateAnswerDisplay(randomWord);
-    let winCheck = "";
+    inputValue.value = "";
+  };
 
-    let tries = 10;
-    triesNumber.innerHTML = `Restam ${tries} tentativas`;
-    triesLetters, (innerHTML = "");
-
-    buttonVerify.onclick = () => {
-      const answerArray = randomWord.split("");
-      let status = false;
-
-      for (let j = 0; j < randomWord.length; j++) {
-        if (inputValue.value.toLowerCase() === answerArray[j]) {
-          wordDisplay[j] = inputValue.value.toLowerCase();
-          wordText.innerHTML = wordDisplay.join(" ");
-          winCheck = wordDisplay.join("");
-          console.log(winCheck);
-          status = true;
-        }
-      }
-      if (!status) {
-        console.log(status);
-        triesLetters.innerHTML += inputValue.value.toLowerCase();
-        tries--;
-
-        drawArray[tries]();
-
-        triesNumber.innerHTML = `Restam ${tries} tentativas`;
-      }
-      if (tries == 0) {
-        // se perder todas as chances
-        triesNumber.innerHTML = "";
-        wordText.innerHTML = "Você perdeu :(";
-        triesNumber.innerHTML = `A palavra era ${randomWord}`;
-      }
-      if (winCheck === randomWord) {
-        wordText.innerHTML = "Ihuul!! Você ganhou :D";
-        triesNumber.innerHTML = "";
-      }
-
-      inputValue.value = "";
-    };
-    inputValue.addEventListener("keypress", (event) => {
-      if (event.key === "Enter") {
-        event.preventDefault();
-        buttonVerify.click();
-      }
-    });
-
-    restartBtn.addEventListener("click", init);
-  }
+  // function animateMan() {
+  //   drawArray[tries]();
+  // }
+  // inputValue.addEventListener("keypress", (event) => {
+  //   if (event.key === "Enter") {
+  //     // event.preventDefault();
+  //     buttonVerify.click();
+  //   }
+  // });
+  restartBtn.addEventListener("click", () => {
+    init();
+  });
 }
+
+}
+function canva() {
+  ctx.fillStyle = "#010b2f";
+  ctx.strokeStyle = "#010b2f";
+  ctx.lineWidth = 10;
+  ctx.lineCap = "round";
+  ctx.beginPath();
+}
+function head() {
+  ctx.beginPath();
+  ctx.arc(200, 80, 30, 0, Math.PI * 2, true);
+  ctx.fill();
+}
+function draw(fromX, fromY, toX, toY) {
+  ctx.moveTo(fromX, fromY);
+  ctx.lineTo(toX, toY);
+  ctx.stroke();
+}
+function verticalTrunk() {
+  draw(30, 10, 30, 300);
+}
+function horizontalTrunk() {
+  draw(10, 10, 220, 10);
+}
+function diagonalTrunk() {
+  draw(30, 60, 80, 10);
+}
+function rope() {
+  draw(200, 10, 200, 50);
+}
+function body() {
+  draw(200, 100, 200, 200);
+}
+function armRight() {
+  draw(200, 120, 250, 150);
+}
+function armLeft() {
+  draw(200, 120, 150, 150);
+}
+function legRight() {
+  draw(200, 200, 170, 260);
+}
+function legLeft() {
+  draw(200, 200, 230, 260);
+}
+
+var drawArray = [
+  legRight,
+  legLeft,
+  body,
+  armLeft,
+  armRight,
+  head,
+  rope,
+  horizontalTrunk,
+  diagonalTrunk,
+  verticalTrunk,
+];
