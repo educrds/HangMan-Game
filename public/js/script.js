af8f8d54a03f82a57be80e28eda802ad6f05b2bc
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-10-09 00:20:27 -0300

    trying use stopImmediatePropagation to fix bug from Enter Button

--- public/js/script.js
+++ public/js/script.js
@@ -161,6 +161,7 @@ function init() {
 
     inputValue.addEventListener("keyup", (event) => {
       if (event.key === "Enter") {
+        event.stopImmediatePropagation();
         buttonVerify.click();
       }
     });
